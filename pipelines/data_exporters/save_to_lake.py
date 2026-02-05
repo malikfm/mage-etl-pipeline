c36fb764a152ab7d3f4e4d41ffddb7dd94f226ca
malikfm <mubarok.malikfajar@gmail.com>
2026-02-05 17:18:05 +0700

    init pipeline

---
+++ pipelines/data_exporters/save_to_lake.py
@@ -0,0 +1,24 @@
+import os
+
+from mage_ai.io.file import FileIO
+from pandas import DataFrame
+
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
+@data_exporter
+def export_data_to_file(df: DataFrame, **kwargs) -> None:
+    """
+    Template for exporting data to filesystem.
+
+    Docs: https://docs.mage.ai/design/data-loading#fileio
+    """
+    execution_date = kwargs['execution_date'].strftime("%Y-%m-%d")
+    table_name = "users" #kwargs['table_name']
+    output_dir = f'data/{table_name}/{execution_date}'
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    filepath = f'{output_dir}/{execution_date}.parquet'
+    FileIO().export(df, filepath, 'parquet')
---
+++ pipelines/data_loaders/extract_from_postgres.py
@@ -0,0 +1,40 @@
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.config import ConfigFileLoader
+from mage_ai.io.postgres import Postgres
+from os import path
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_postgres(*args, **kwargs):
+    """
+    Template for loading data from a PostgreSQL database.
+    Specify your configuration settings in 'io_config.yaml'.
+
+    Docs: https://docs.mage.ai/design/data-loading#postgresql
+    """
+    execution_date = kwargs['execution_date'].strftime("%Y-%m-%d")
+    table_name = "users" #kwargs['table_name']
+    selected_columns = "*" #kwargs['selected_columns']
+
+    query = f"""
+    SELECT {selected_columns} FROM {table_name}
+    WHERE created_at::DATE = '{execution_date}'
+    OR updated_at::DATE = '{execution_date}'
+    """
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+    config_profile = 'source'
+
+    with Postgres.with_config(ConfigFileLoader(config_path, config_profile)) as loader:
+        return loader.load(query)
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
